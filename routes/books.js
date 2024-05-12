7c2118adc30ce826d7a0a15ce660a2212bcbd662
Yuvraj Rajak <yuvrajrajak538@gmail.com>
2024-05-12 13:03:17 +0530

    books create/index/new

--- routes/books.js
+++ routes/books.js
@@ -23,6 +23,12 @@ router.get('/', async (req, res) => {
   if(req.query.title != null && req.query.title != '' ){
     query = query.regex('title', new RegExp(req.query.title,'i'))
   }
+  if(req.query.publishedBefore != null && req.query.publishedBefore != '' ){
+    query = query.lte('publishDate',req.query.publishedBefore)
+  }
+  if(req.query.publishedAfter != null && req.query.publishedAfter != '' ){
+    query = query.gte('publishDate',req.query.publishedAfter)
+  }
 
   try {
       const books = await query.exec()
