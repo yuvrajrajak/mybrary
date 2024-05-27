299c160e9de60e9c29c94ba6447b3d95d54a12a7
Yuvraj Rajak <yuvrajrajak538@gmail.com>
2024-05-27 17:23:47 +0530

    added general styles

--- routes/authors.js
+++ routes/authors.js
@@ -23,7 +23,7 @@ router.get('/', async (req, res) => {
 // New Author Route
 
 router.get('/new', (req, res) => {
-    res.render('authors/new', { author: new Author() })
+    res.render('authors/new',{ author: new Author() })
 })
 
 // Create Author Route
